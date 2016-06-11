14f2b902411e33361e47bdbf1b3642d0097e4f1f
Dmitriy Dubson <d.dubson@gmail.com>
2016-06-11 18:30:52 -0400

    Added day1 code

---
+++ day1_readfromstdin.py
@@ -0,0 +1,17 @@
+__name__="main"
+
+def main():
+    i = 4
+    d = 4.0
+    s = 'HackerRank '
+
+    myint = int(raw_input())
+    mydouble = float(raw_input())
+    mystr = raw_input()
+
+    print myint + i
+    print mydouble + d
+    print mystr + s
+
+if __name__ == "main":
+    main()

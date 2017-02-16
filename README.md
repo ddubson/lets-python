b73608cf7de0fb600660343d1b793360da044019
Dmitriy Dubson <d.dubson@gmail.com>
2017-02-15 21:08:50 -0500

    Added fuzzing example

--- README.md
+++ README.md
@@ -6,6 +6,10 @@
 
 `networking` module features examples on Networking
 
+Note:
+> For fuzzing.py, run `pip install http://effbot.org/downloads/Imaging-1.1.6.tar.gz` to get the right dependency for the
+> imaging library
+
 # Operating Systems
 
 `os` module features examples operating systems calls.
--- networking/fuzzing.py
+++ networking/fuzzing.py
@@ -1,12 +1,15 @@
+# https://www.owasp.org/index.php/Fuzzing
+
 from pyfuzz.generator import random_ascii
 import socket
 
-msg = random_ascii + b" / HTTP/1.1" \
-                               b"\nHost: localhost\r\n"
+msg = random_ascii() + b" / HTTP/1.1\nHost: localhost\r\n"
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print(msg)
+
 try:
-    addr = ("localhost", 8080)
+    addr = ("localhost", 80)
     s.connect(addr)
     s.sendall(msg)
     resp = s.recv(4096)
